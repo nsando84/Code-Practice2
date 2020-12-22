ad8e4d38c94ba30b627c1edc4b8aa0c05b61beb5
nicksandoval <sand8val@gmail.com>
2020-12-21 23:39:24 -0800

    added more working

--- expressmod/app.js
+++ expressmod/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -19,7 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-const errorController = require('./controllers/error');
 app.use(errorController.get404);
 
 app.listen(3000);
