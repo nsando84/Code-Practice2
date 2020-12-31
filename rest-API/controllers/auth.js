18048de5899428d37aae0c1e1258730c10842170
nicksandoval <sand8val@gmail.com>
2020-12-31 12:09:46 -0800

    asyn/await

--- rest-API/controllers/auth.js
+++ rest-API/controllers/auth.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
     
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -16,45 +16,42 @@ exports.signup = (req, res, next) => {
     const name = req.body.name;
     const password = req.body.password;
 
-    
-
-    bcrypt.hash(password, 12)
-        .then(hashedPw => {
+    try {
+    const hashedPw = await bcrypt.hash(password, 12)
+      
             const user = new User({
                 email: email,
                 password: hashedPw,
-                name: name
+                name: name    
             })
-            return user.save()
-        })
-        .then(result => {
-            res.status(201).json({message: 'user created!', userId: result._id})
-        })
-        .catch(err => {
+
+        const result = await user.save()
+        res.status(201).json({message: 'user created!', userId: result._id})
+      
+    } catch(err) {
             if (!err.statusCode) {
                 err.statusCode = 500;
               }
               next(err);
-        })
+    }
 };
 
-exports.login = ( req, res, next ) => {
+exports.login = async ( req, res, next ) => {
     const email = req.body.email;
     const password = req.body.password;
     let loadedUser;
 
-    User.findOne({email: email})
-        .then(user => {
+    try {
+    const user = await User.findOne({email: email})
+     
             if (!user) {
                 const error = new Error('User not found.');
                 error.statusCode = 401;
                 throw error;
             }
             loadedUser = user            
-            return bcrypt.compare(password, user.password)
-
-        })
-        .then(isEqual => {
+            const isEqual = await bcrypt.compare(password, user.password)
+        
             if (!isEqual) {
                 const error = new Error('Wrong password.');
                 error.statusCode = 401;
@@ -67,18 +64,19 @@ exports.login = ( req, res, next ) => {
             }, 'my secret', { expiresIn: '1h' })
             res.status(200).json({token: token, userId: loadedUser._id.toString()})
 
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-              }
-              next(err);
-        })
+       
+    } catch(err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+            next(err);
+    }
 }
 
-exports.getUserStatus = (req, res, next) => {
-    User.findById(req.userId)
-        .then(user => {
+exports.getUserStatus = async (req, res, next) => {
+    try {
+    const user = await User.findById(req.userId)
+  
             if (!user) {
                 const error = new Error('user not found.');
                 error.statusCode = 404;
@@ -87,19 +85,21 @@ exports.getUserStatus = (req, res, next) => {
             res.status(200).json({
                 status: user.status
             })
-        })
-        .catch(err => {
+
+    } catch(err) {
             if (!err.statusCode) {
                 err.statusCode = 500;
               }
               next(err);
-        })
+    }
 }
 
-exports.updateUserStatus = (req, res, next) => {
+exports.updateUserStatus = async (req, res, next) => {
     const newStatus = req.body.status;
-    User.findById(req.userId)
-        .then(user => {
+
+    try {
+    const user = await User.findById(req.userId)
+     
             if (!user) {
                 const error = new Error('user not found.');
                 error.statusCode = 404;
@@ -107,16 +107,13 @@ exports.updateUserStatus = (req, res, next) => {
             };
 
             user.status = newStatus;
-            return user.save()
-
-        })
-        .then(result => {
+            await user.save()
             res.status(200).json({message: 'user updated.'})
-        })
-        .catch(err => {
+      
+    } catch(err) {
             if (!err.statusCode) {
                 err.statusCode = 500;
               }
               next(err);
-        })
+    }
 }
