11019088dcaea867bd66599d0ebfe1b2f43b78de
nicksandoval <sand8val@gmail.com>
2020-12-30 20:53:48 -0800

    finished rest api lecture

--- rest-API/app.js
+++ rest-API/app.js
@@ -10,6 +10,7 @@ const MONGODB_URI =
 
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -38,7 +39,10 @@ app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req ,res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH')
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+      );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()
 })
@@ -46,12 +50,15 @@ app.use((req ,res, next) => {
 
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
 
 app.use((error, req, res, next) => {
     console.log(error)
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({message: message})
+    const data = error.data
+    res.status(status).json({ message: message, data: data })
 })
 
 mongoose.connect(MONGODB_URI)
--- rest-API/routes/feed.js
+++ rest-API/routes/feed.js
@@ -4,8 +4,9 @@ const { body } = require('express-validator/check')
 const router = express.Router();
 
 const feedController = require('../controllers/feed')
+const isAuth = require('../middleware/is-auth')
 
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 router.post('/post', 
     [
@@ -16,8 +17,24 @@ router.post('/post',
             .trim()
             .isLength({ min: 5 })
     ], 
+    isAuth,
 feedController.createPost);
 
-router.get('/post/:postId', feedController.getPost)
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.put('/post/:postId', 
+    [
+        body('title')
+            .trim()
+            .isLength({ min: 5 }),
+        body('content')
+            .trim()
+            .isLength({ min: 5 })
+    ], 
+    isAuth,
+    feedController.updatePost)
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
+
 
 module.exports = router;
